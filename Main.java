a40b0f6b8dcbefe86d089643d2375f28bc761726
radhikaN <radhika@gmail.com>
2022-04-07 17:20:46 +0530

    this are the new changes made by Radhika

--- Main.java
+++ Main.java
@@ -2,14 +2,16 @@ class Main {
 
   public static void main(String[] args) {
     
-    System.out.println("Enter two numbers");
+    System.out.println("Enter four numbers");
     int first = 10;
     int second = 20;
+	int third = 30;
+	int forth = 40;
     
-    System.out.println(first + " " + second);
+    System.out.println(first + " " + second + " " + third + " " + forth);
 
-    // add two numbers
-    int sum = first + second;
+    // add four numbers
+    int sum = first + second + third + forth;
     System.out.println("The sum is: " + sum);
   }
 }
