3973667eda2543f4a37e101a311173d736a30a81
radhikaN <radhika@gmail.com>
2022-04-08 15:00:59 +0530

    this is changed code

--- java2.java
+++ java2.java
@@ -2,7 +2,7 @@ class java2 {
   public static void main(String[] args) {
 
     // create a string
-    String greet = "Hello! World";
+    String greet = "Hello! World this is string operations";
     System.out.println("String: " + greet);
 
     // get the length of greet
